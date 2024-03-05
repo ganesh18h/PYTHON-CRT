4bb67ac5cca02e185322c771ac5a636f5fdd6d3e
ganesh18h <120293348+ganesh18h@users.noreply.github.com>
2024-03-05 15:11:08 +0530

    https://github.com/abi/screenshot-to-code.git

---
+++ CRT DAY 2.py
@@ -0,0 +1,12 @@
+age=int(input())
+if age == 0:
+    print("ALREADY DIED")
+elif age <=10:
+    print("BOY")
+elif age <=60:
+    print("MAN")
+elif age <=100:
+    print("ELDER")
+else:
+    print("IMMORTAL")
+
